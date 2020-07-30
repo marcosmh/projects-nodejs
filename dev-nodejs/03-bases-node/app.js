174bb94ef60adf4d1673a83a0cd8c22ff73cac29
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2020-07-29 22:44:34 -0500

    pasando argumentos en una funcion node

--- dev-nodejs/03-bases-node/app.js
+++ dev-nodejs/03-bases-node/app.js
@@ -1,13 +1,21 @@
 
+const argv = require('yargs').
+    command('listar','Imprime en la consola la tabla de multiplicar',{
+        base: {
+          demand: true,
+          alias: 'b'
+        }
+    })
+    .argv;
 
 const { crearArchivo } = require('./multiplicar/multiplicar.js');
 
 //console.log(process.argv);
 // node app --base10
 
-let argv      = process.argv;
-let parametro = argv[2];
-let base      = parametro.split("=")[1] ? parametro.split("=")[1] === undefined : "error";
+//let argv      = process.argv;
+//let parametro = argv[2];
+//let base      = parametro.split("=")[1] ? parametro.split("=")[1] === undefined : "error";
 
 console.log(base);
 
