8520ee795289af7b4a5bb9150b4c049dc7577a14
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2020-08-01 21:27:07 -0500

    agregar funcion listar

--- dev-nodejs/03-bases-node/app.js
+++ dev-nodejs/03-bases-node/app.js
@@ -13,17 +13,29 @@ const argv = require('yargs')
     .help()
     .argv;
 
-const { crearArchivo } = require('./multiplicar/multiplicar.js');
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar.js');
+let comando = argv._[0];
 
 //console.log(process.argv);
 // node app --base10
-
 //let argv      = process.argv;
 //let parametro = argv[2];
 //let base      = parametro.split("=")[1] ? parametro.split("=")[1] === undefined : "error";
 
-console.log(base);
 
-crearArchivo(base)
-  .then(archivo => console.log(`El archivo ${archivo} ha sido creado.`))
-  .catch(err => console.log("Error: ",err));
+switch(comando) {
+  case 'listar':
+    console.log(`Lista de la Tabla del Núm. ${argv.base}`);
+    listarTabla(argv.base,argv.limite)
+      .then(listar => console.log(listar))
+      .catch(err => console.log('Error'));
+    break;
+  case 'crear':
+    console.log('crear');
+    crearArchivo(argv.base)
+      .then(archivo => console.log(`El archivo ${archivo} ha sido creado.`))
+      .catch(err => console.log("Error: ",err));
+    break;
+  default:
+    console.log('Comando no reconocido');
+}
--- dev-nodejs/03-bases-node/multiplicar/multiplicar.js
+++ dev-nodejs/03-bases-node/multiplicar/multiplicar.js
@@ -1,5 +1,31 @@
 const fs = require('fs');
 
+
+let listarTabla = (base, limite) => {
+  
+  return new Promise((resolve,reject) => {
+
+    if(!Number(base)) {
+      reject(`El valor introducido ${base} no es un número`);
+      return;
+    }
+
+    if(!Number(limite)) {
+      reject(`El valor introducido ${limite} no es un número`);
+      return;
+    }
+
+    for(let i = 1; i <= limite; i++) {
+        console.log(`${i} * ${base} =`,i * base);
+        resolve += `${i} * ${base} =`,i * base;
+    }
+
+
+  });
+
+
+}
+
 let crearArchivo = (base) => {
     console.log("base: "+base);
     return new Promise((resolve,reject) => {
@@ -27,5 +53,6 @@ let crearArchivo = (base) => {
 }
 
 module.exports = {
-  crearArchivo
+  crearArchivo,
+  listarTabla
 }
