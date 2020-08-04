b4ba1f8f208144bb3a5400d53feff506590c6722
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2020-08-04 00:40:04 -0500

    peticiones con http - axios - request

--- dev-nodejs/05-clima-mundo/app.js
+++ dev-nodejs/05-clima-mundo/app.js
@@ -1,5 +1,17 @@
+
+const axios = require('axios');
 const argv = require('./config/yargs').argv;
+const encodeUrl = encodeURI(argv.direccion);
 
+const instance = axios.create({
+  baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeUrl}`,
+  headers: {'X-RapidAPI-Key': 'ed9cb87849mshb5c97d5450f803cp126c8ejsn99f98fa43cad'}
+});
 
-
-console.log(argv.direccion);
+instance.get()
+    .then(result => {
+        console.log(result.data.Results);
+    })
+    .catch(error => {
+        console.log("Error: ",error);
+    })
