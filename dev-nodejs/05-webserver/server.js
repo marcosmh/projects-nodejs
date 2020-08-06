10e3c9da9b1d27696b054435a845b9cf82425d4a
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2020-08-05 23:20:07 -0500

    instalando handlebars

--- dev-nodejs/05-webserver/server.js
+++ dev-nodejs/05-webserver/server.js
@@ -4,16 +4,16 @@ const app = express();
 
 app.use(express.static(__dirname +  '/public'));
 
-// app.get('/', (req, res) => {
-//     let salida = {
-//         nombre: 'Marcos',
-//         apellido: 'Magaña',
-//         url: req.url
-//     }
-//
-//   res.send(salida);
-//
-// });
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+
+  res.render('home',{
+    nombre: 'Marcos',
+    anio: new Date().getFullYear()
+  });
+
+});
 
 app.listen(3000, ()=> {
   console.log('Escuchando en el puerto: 3000');
