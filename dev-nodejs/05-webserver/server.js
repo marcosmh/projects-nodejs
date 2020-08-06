615d5e234d578eb098136b0047d37576150b6fbd
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2020-08-05 23:52:32 -0500

    usando parciales con hbs

--- dev-nodejs/05-webserver/server.js
+++ dev-nodejs/05-webserver/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-
+const hbs = require('hbs');
 
 app.use(express.static(__dirname +  '/public'));
 
+//Express HBS Engine
+
+hbs.registerPartials(__dirname +  '/views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
@@ -13,6 +16,12 @@ app.get('/', (req, res) => {
     anio: new Date().getFullYear()
   });
 
+});
+
+app.get('/about', (req, res) => {
+  res.render('about',{
+    anio: new Date().getFullYear()
+  });
 });
 
 app.listen(3000, ()=> {
